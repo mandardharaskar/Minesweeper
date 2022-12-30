9fa354f6872eaf8d9808b26301ebef074cd75140
Mandar D <38375621+mandardharaskar@users.noreply.github.com>
2022-12-30 20:31:36 +0530

    Update

--- Minesweeper/main.c
+++ Minesweeper/main.c
@@ -6,6 +6,7 @@ const int max_x_y[64] = {0};
 void vInit(void);
 void vDrawGameBlock(void);
 void vAddMine(void);
+void vPlayGame(void);
 void vSelectGameBlock(void);
 
 static int sbOption = 0;
@@ -17,7 +18,30 @@ int C[5]= {0};
 bool FlagR = false;
 bool FlagC = false;
 
-int ScanCount = 4;
+int msb = 0;
+int lsb = 0;
+
+bool InputErrFlag = false;
+bool bSetMineFlag = false;
+int temp,tempprev =0;
+int RSCount =1;
+int ScanCount = 5;
+
+bool InputErrFlag2 = false;
+bool bSetMineFlag2 = false;
+int temp2,tempprev2 =0;
+int RSCount2 =1;
+int ScanCount2 = 5;
+bool bRepeatInputFlag =false;
+bool bGameOverFlag =false;
+
+
+int PB[60]= {0};//play Block Input
+int PBR[60]= {0};//Play block Row
+int PBC[60]= {0};//Play Block column
+
+int a,b,c=1;
+int x,y=1;
 
 enum{
     R0 = 0,
@@ -59,7 +83,7 @@ void vInit(void)
 {
     //Game_Modes xT_Play;
 
-    printf("Select the Below Choice from the Number :\n");
+    printf("\nSelect the Below Choice from the Number :\n");
     printf("1. Add Mines.\n");
     printf("2. Play Game.\n");
     printf("3. Test Case.\n");
@@ -71,10 +95,11 @@ void vInit(void)
     {
         case MINE:
             vAddMine();
+            vInit();
             break;
 
         case PLAY:
-            vSelectGameBlock();
+            vPlayGame();//vSelectGameBlock();
             break;
 
         case TEST:
@@ -98,15 +123,6 @@ int sbSetOptionsNo(int sbOpt)
     return sbCase;
 }
 
-int msb = 0;
-int lsb = 0;
-
-bool InputErrFlag = false;
-int temp =0;
-int RSCount =0;
-
-int a,b,c=0;
-int x,y=0;
 
 void vAddMine(void)
 {
@@ -115,19 +131,19 @@ void vAddMine(void)
     vDrawGameBlock();
 
     printf("\n Choose 5 RC combination to Add Mine : \n");
-    for(int i = 0;i<=ScanCount;i++)
+    for(int i = 1;i<=ScanCount;i++)
     {
         scanf("%d",&temp);
-        if(temp == m[0] || temp == m[1] || temp == m[2] || temp == m[3])
-        {
-            InputErrFlag = true;
-            printf("Entered Row column is Repeated. Select different RC \n");
-            ScanCount = ScanCount+1;
-        }
-         else if(temp>77)
+        if((temp>88) || (temp == 0) || (temp%10 == 9) || (temp%10 == 0))//check the column is not 9th
         {
             InputErrFlag = true;
             printf("Please choose the Correct Row & Column \n");
+            ScanCount = ScanCount+1;
+        }
+        else if(temp == m[1] || temp == m[2] || temp == m[3] || temp == m[4])
+        {
+            InputErrFlag = true;
+            printf("Entered Row column is Repeated. Select different RC \n");
             ScanCount = ScanCount+1;
         }
         else{
@@ -138,13 +154,15 @@ void vAddMine(void)
         {
             m[RSCount] = temp;
             RSCount = RSCount + 1;
-            if(RSCount >4)
+            if(RSCount >5)
                 RSCount = RSCount-1;
+
         }
 
     }
+
 /***********Code for debugging**********/
-    for(int i = 0;i<=4;i++)
+    for(int i = 1;i<=5;i++)
     {
         R[i] = m[i]/10;//Row//msb
         C[i] = m[i]%10;//Column//lsb
@@ -154,16 +172,29 @@ void vAddMine(void)
 
     printf("\n");
 /***************************************/
-    for(int b=0;b<8;b++)
+    for(int b=1;b<9;b++)
         printf("C%d ",b);
 
     printf("\n");
 
-    for(x=0;x<8;x++)
+    for(x=1;x<9;x++)
     {
-        for(y=0;y<8;y++)
+        for(y=1;y<9;y++)
         {
-            if(x==R[x] && y==C[y])
+            if(x==R[1] && y==C[1])
+                bSetMineFlag = true;
+            else if(x==R[2] && y==C[2])
+                bSetMineFlag = true;
+            else if(x==R[3] && y==C[3])
+                bSetMineFlag = true;
+            else if(x==R[4] && y==C[4])
+                bSetMineFlag = true;
+            else if(x==R[5] && y==C[5])
+                bSetMineFlag = true;
+            else
+                bSetMineFlag = false;
+
+            if(bSetMineFlag)
                 printf(" M ");
             else
                 printf(" * ");
@@ -173,16 +204,133 @@ void vAddMine(void)
 
 }
 
+void vPlayGame(void)
+{
+  vDrawGameBlock();
+
+    for(int i = 1;i<=ScanCount2;i++)
+    {
+        printf("\nPlease Enter Row & Column \n");
+
+        scanf("%d",&temp2);
+
+        for(int a=1;a<RSCount2;a++)
+        {
+            if(temp2 == PB[a])
+                bRepeatInputFlag = true;
+        }
+
+        if((temp2>88) || (temp2 == 0))
+        {
+            InputErrFlag2 = true;
+            printf("Please choose the Correct Row & Column \n");
+            ScanCount2 = ScanCount2+1;
+        }
+        else if(bRepeatInputFlag == true)
+        {
+            InputErrFlag2 = true;
+            printf("Entered Row column is Repeated. Select different RC \n");
+            ScanCount2 = ScanCount2+1;
+        }
+        else{
+            InputErrFlag2 = false;
+        }
+
+        if(InputErrFlag2 == false)
+        {
+            PB[RSCount2] = temp2;
+
+//--------------Calculate the selected block---------------------------
+/***********Code for debugging**********/
+ //           for(int i = 1;i<=RSCount2;i++)
+ //           {
+            PBR[RSCount2] = PB[RSCount2]/10;//Row//msb
+            PBC[RSCount2] = PB[RSCount2]%10;//Column//lsb
+            printf("\n PB : %d ",PB[RSCount2]);
+            printf(" msb is %d & lsb is %d \n",PBR[RSCount2],PBC[RSCount2]);
+//            }
+            printf("\n");
+/***************************************/
+//--------------if input is right draw new block with no. around the block--------
+            for(int b=1;b<9;b++)
+                printf("C%d ",b);
+
+            printf("\n");
+
+            for(x=1;x<9;x++)
+            {
+                for(y=1;y<9;y++)
+                {
+                    if((x==R[1]==PBR[RSCount2]) && (y==C[1]==PBR[RSCount2]))
+                    {
+                        bGameOverFlag = true;
+                        bSetMineFlag = true;
+                    }
+                    else if((x==R[2]==PBR[RSCount2]) && (y==C[2]==PBR[RSCount2]))
+                    {
+                        bGameOverFlag = true;
+                        bSetMineFlag = true;
+                    }
+                    else if((x==R[3]==PBR[RSCount2]) && (y==C[3]==PBR[RSCount2]))
+                    {
+                        bGameOverFlag = true;
+                        bSetMineFlag = true;
+                    }
+                    else if((x==R[4]==PBR[RSCount2]) && (y==C[4]==PBR[RSCount2]))
+                    {
+                        bGameOverFlag = true;
+                        bSetMineFlag = true;
+                    }
+                    else if((x==R[5]==PBR[RSCount2]) && (y==C[5]==PBR[RSCount2]))
+                    {
+                        bGameOverFlag = true;
+                        bSetMineFlag = true;
+                    }
+                    else
+                        bSetMineFlag = false;
+
+                    if(bSetMineFlag)
+                    {
+                        printf(" M ");
+                    }
+                    else if(!bSetMineFlag)
+                    {
+                        printf(" * ");
+
+                    }
+                }
+                printf("R%d \n",x);
+            }
+
+            RSCount2 = RSCount2 + 1;// increase the index after every input
+
+            if(bGameOverFlag)
+            {
+                printf("\n*Game Over* \n");
+                ScanCount2 =0;
+                break;
+            }
+
+            if(RSCount2 > 60)
+                RSCount2 = RSCount2-1;
+
+        }
+
+    }
+
+
+}
+
 void vDrawGameBlock(void)
 {
-    for(int b=0;b<8;b++)
+    for(int b=1;b<9;b++)
         printf("C%d ",b);
 
     printf("\n");
 
-    for(int i=0;i<8;i++)//Row
+    for(int i=1;i<9;i++)//Row
     {
-        for(int j=0;j<8;j++)//Column
+        for(int j=1;j<9;j++)//Column
         {
             printf(" * ");
         }
